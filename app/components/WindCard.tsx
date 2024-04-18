76b7e0a7c5888852d37042a98f0e0179356f707a
Hasib Al Rashid <hasibrashid3091@gmail.com>
2024-04-18 11:25:49 +0600

    Wind Done

--- app/components/WindCard.tsx
+++ app/components/WindCard.tsx
@@ -1,16 +1,17 @@
 import { Card } from "@mantine/core";
 import { IconWind } from "@tabler/icons-react";
+import Compass from "./Compass";
 
 export default function WindCard() {
     return (
         <div>
             <Card className="w-full h-[39vh] pb-9 text-white bg-[#1a1a1a] rounded-lg" >
                 {/* Sunrise */}
-                <div className="flex items-center mb-3">
+                <div className="flex items-center mb-12">
                     <IconWind />
                     <h3 className="text-[17px] ml-2 font-semibold">Wind</h3>
                 </div>
-
+                <Compass deg={170} speed={2.5} />
             </Card>
         </div>
     )
