cd62227707c0416ae14ccbd22911aab82aebfa3b
Hasib Al Rashid <hasibrashid3091@gmail.com>
2024-04-24 13:55:14 +0600

    Remove Date function

--- app/components/Forecast.tsx
+++ app/components/Forecast.tsx
@@ -46,17 +46,3 @@ export default function Forecast(forecast: any) {
         </div>
     )
 }
-
-function convertToDate(
-    timezone: number,
-    dt: number,
-    weekdayFormat: "short" | "long"
-): string {
-    let utc_time = new Date(dt * 1000)
-    let local_time = new Date(utc_time.getTime() + timezone * 1000)
-
-    const options = { weekday: weekdayFormat }
-    const dateFormatter = new Intl.DateTimeFormat("UTC", options)
-
-    return dateFormatter.format(local_time)
-}
